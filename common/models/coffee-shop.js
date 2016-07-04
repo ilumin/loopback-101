00d586aba63ce3a58499c1419fa94062837f9fac
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-07-04 11:09:22 +0700

    UPDATE coffee shop remote method

--- common/models/coffee-shop.js
+++ common/models/coffee-shop.js
@@ -1,3 +1,25 @@
-module.exports = function(Coffeeshop) {
+module.exports = function(CoffeeShop) {
+  CoffeeShop.status = function(callback) {
+    var currentDate = new Date();
+    var currentHour = currentDate.getHours();
+    var OPEN_HOUR = 6;
+    var CLOSE_HOUR = 20;
 
+    console.log('Current hour is ' + currentHour);
+
+    var response;
+    if (currentHour > OPEN_HOUR && currentHour < CLOSE_HOUR) {
+      response = 'We are open for business.';
+    }
+    else {
+      response = 'Sorry, we are closed. Open daily from 6am to 8pm.';
+    }
+
+    callback(null, response);
+  };
+
+  CoffeeShop.remoteMethod('status', {
+    http: { path: '/status', verb: 'get' },
+    returns: { arg: 'status', type: 'string' }
+  });
 };
